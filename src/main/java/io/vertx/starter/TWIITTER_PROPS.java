677ba9d83f52f0796bc2ffdb589239f29f1a0912
jeremyrdavis <jeremyrdavis@jeremyrdavis.com>
2019-03-28 11:46:26 -0400

    pulling properties

---
+++ src/main/java/io/vertx/starter/TWIITTER_PROPS.java
@@ -0,0 +1,7 @@
+package io.vertx.starter;
+
+public class TWIITTER_PROPS {
+
+  static final String oauth_consumerKey = "oauth.consumerKey";
+  static final String oauth_token = "oauth.consumerSecret";
+}
